0b23ac526fd8b8c0066d722c46ebb083634b0ee7
Sourabh Munde <sourabh.munde15@gmail.com>
2024-08-08 20:19:03 +0530

    update: api endpoint

--- src/SpeechToText.js
+++ src/SpeechToText.js
@@ -135,7 +135,7 @@ const SpeechToText = () => {
     setIsLoading(true); // Start loading
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/process_command", {
+      const response = await fetch("https://ivca.nexpictora.com/process_command", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ data: spokenText }),
